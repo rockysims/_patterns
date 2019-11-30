59226e1385301f230b6d3c9096bc130bf9504260
Rocky Sims <rocky.sims@gmail.com>
2019-11-30 05:28:13 -0800

    More debugging index.php file.

--- index.php
+++ index.php
@@ -20,11 +20,13 @@
 			$allChildPaths = [];
 
 			if (is_dir($curPath)) {
+				var_dump(scandir($curPath));
 				foreach (scandir($curPath) as $child) {
 					if ($child === '.' || $child === '..') continue;
 					$allChildPaths->array_push(getDirPaths($curPath . '/'. $child));
 				}
 			} else {
+				echo "found file $curPath<br/>";
 				$allChildPaths[] = $curPath;
 			}
 

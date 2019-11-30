9230d70f2c9e164ec0014c456d7e52248cff27ac
Rocky Sims <rocky.sims@gmail.com>
2019-11-30 06:28:22 -0800

    More tweaking how index.php renders.

--- index.php
+++ index.php
@@ -16,17 +16,21 @@
 		<br/>
 		<?php
 		function renderIndex($curPath) {
-			$file = basename($curPath);
-			$indent = str_repeat("&nbsp;&nbsp;&nbsp;&nbsp;", substr_count($curPath, "/"));
+			if (!is_dir($curPath)) throw "Error: '$curPath' is not a directory.";
 
-			if (is_dir($curPath)) {
-				foreach (scandir($curPath) as $child) {
-					if ($child === '.' || $child === '..') continue;
+			$indent = str_repeat("&nbsp;&nbsp;&nbsp;&nbsp;", substr_count($curPath, "/"));
+			$file = basename($curPath);			
+
+			foreach (scandir($curPath) as $child) {
+				if ($child === '.' || $child === '..') continue;
+
+				$childPath = $curPath . '/' . $child;
+				if (is_dir($childPath)) {
 					echo "$indent <strong>$file/</strong><br/>";
-					renderIndex($curPath . '/' . $child);
+					renderIndex($childPath);
+				} else {
+					echo "$indent <a href=\"$curPath\">$file</a><br/>";
 				}
-			} else {
-				echo "$indent <a href=\"$curPath\">$file</a><br/>";
 			}
 		}
 

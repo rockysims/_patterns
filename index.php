fc9d1e1442ff59695cdf3ff33abf9b6dec9c07a8
Rocky Sims <rocky.sims@gmail.com>
2019-11-30 04:04:36 -0800

    Part way through making index.php have links to vanilla/* files.

--- index.php
+++ index.php
@@ -1,1 +1,64 @@
-<?php include_once("home.html") ?>
+<?php //include_once("home.html") ?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>_patterns</title>
+    </head>
+    <body>
+        Confused? See 
+        <a href="https://github.com/rockysims/_patterns/blob/master/README.md">
+            https://github.com/rockysims/_patterns/blob/master/README.md
+        </a>
+        <br/>
+		<br/>
+		<?php
+
+
+
+		$dirPath = 'learningMaterial/code/vanilla';
+
+		$files = array();
+		foreach (scandir($dirPath) as $file) {
+			if ($file === '.' || $file === '..') continue;
+			$files[] = $file;
+		}
+
+		var_dump($files);
+
+
+
+
+
+		// $path = "learningMaterial/code/vanilla";
+
+		// if ($handle = opendir($path)) {
+		// 	while (false !== ($file = readdir($handle))) {
+		// 		if ('.' === $file) continue;
+		// 		if ('..' === $file) continue;
+		
+		// 		//render link
+		// 		echo $file->basename();
+		// 	}
+		// 	closedir($handle);
+		// }
+
+
+
+		// $dir = new DirectoryIterator('learningMaterial/code/vanilla');
+		// foreach ($dir as $fileinfo) {
+		// 	if (!$fileinfo->isDot()) {
+		// 		var_dump($fileinfo->getFilename());
+		// 	}
+		// }
+		?>
+        <a href="learningMaterial/code/vanilla/_start.html">
+            learningMaterial/code/vanilla/_start.html
+        </a>
+        <a href="learningMaterial/code/vanilla/zz_debugger.html">
+            learningMaterial/code/vanilla/zz_debugger.html
+        </a>
+    </body>
+</html>

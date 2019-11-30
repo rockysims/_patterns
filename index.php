34b65557a19d16cf34654b3b7786b96bc55fd66b
Rocky Sims <rocky.sims@gmail.com>
2019-11-30 06:04:52 -0800

    Refactoring and adding directory names to links in index.php file.

--- index.php
+++ index.php
@@ -15,28 +15,57 @@
         <br/>
 		<br/>
 		<?php
-		function getDirPaths($curPath) {
-			$allChildPaths = [];
+		function renderIndex($curPath) {
+			$dir = dirname($curPath);
+			$file = basename($curPath);
+			$depth = substr_count($curPath, "/");
+			$indent = str_repeat("&nbsp;&nbsp;", $depth);
 
 			if (is_dir($curPath)) {
 				foreach (scandir($curPath) as $child) {
 					if ($child === '.' || $child === '..') continue;
-					array_push($allChildPaths, ...getDirPaths($curPath . '/' . $child));
+					echo "$indent $file<br/>";
+					getDirPaths($curPath . '/' . $child);
 				}
 			} else {
-				$allChildPaths[] = $curPath;
+				echo "$indent <a href=\"$curPath\">$file</a><br/>";
 			}
-
-			return $allChildPaths;
 		}
 
-		foreach (getDirPaths('learningMaterial') as $path) {
-			$dir = dirname($path);
-			$file = basename($path);
-			$depth = substr_count($path, "/");
-			$indent = str_repeat("&nbsp;&nbsp;", $depth);
-			echo "<a href=\"$path\">$indent $file</a><br/>";
-		}
+		renderIndex('learningMaterial');
+
+
+
+
+
+
+
+
+
+
+
+		// function getDirPaths($curPath) {
+		// 	$allChildPaths = [];
+
+		// 	if (is_dir($curPath)) {
+		// 		foreach (scandir($curPath) as $child) {
+		// 			if ($child === '.' || $child === '..') continue;
+		// 			array_push($allChildPaths, ...getDirPaths($curPath . '/' . $child));
+		// 		}
+		// 	} else {
+		// 		$allChildPaths[] = $curPath;
+		// 	}
+
+		// 	return $allChildPaths;
+		// }
+
+		// foreach (getDirPaths('learningMaterial') as $path) {
+		// 	$dir = dirname($path);
+		// 	$file = basename($path);
+		// 	$depth = substr_count($path, "/");
+		// 	$indent = str_repeat("&nbsp;&nbsp;", $depth);
+		// 	echo "$indent<a href=\"$path\">$file</a><br/>";
+		// }
 		?>
     </body>
 </html>
